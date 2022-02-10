8dbf71814db44d62c4d545617eb22854de23e776
Christian Vazquez <vazquez.a.christian36@gmail.com>
2022-02-10 12:36:44 -0600

    added a timeout to the loading

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-fetch("https://perpetual-pear-bearberry.glitch.me/movies")
-    .then(response => response.json())
-    .then(movies => {
-        movies.forEach(movie => {
-            console.log(movie);
-            let title = movie.title
-            let img = movie.poster
-            let director = movie.director
-            let actors = movie.actors
-            let genre = movie.genre
-            let year = movie.year
-            let plot = movie.plot
-            let rating = movie.rating
-            $(".card-deck").append(`
-                <div class="card" style="width: 18rem">
-                    <img class="card-img-top" src="${img}" alt="movie poster">
-                    <div class="card-body">
-                        <h5 class="card-title">${title}</h5>
-                        <p class="card-text">${plot}</p>
+const apiList = "https://perpetual-pear-bearberry.glitch.me/movies"
+
+getMovies()
+function getMovies() {
+    setTimeout(() => {
+    fetch(apiList)
+        .then(response => response.json())
+        .then(movies => {
+            $(".loader").hide();
+            movies.forEach(movie => {
+                console.log(movie);
+                let title = movie.title
+                let img = movie.poster
+                let director = movie.director
+                let actors = movie.actors
+                let genre = movie.genre
+                let year = movie.year
+                let plot = movie.plot
+                let rating = movie.rating
+                $(".card-deck").append(`
+                    <div class="card" style="width: 18rem">
+                        <img class="card-img-top" src="${img}" alt="movie poster">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p class="card-text">${plot}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Dir: ${director}, ${year}</li>
+                            <li class="list-group-item">${actors}</li>
+                            <li class="list-group-item">Rating: ${rating}</li>
+                        </ul>
+                        <div class="card-footer">${genre}</div>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Dir: ${director}, ${year}</li>
-                        <li class="list-group-item">${actors}</li>
-                        <li class="list-group-item">Rating: ${rating}</li>
-                    </ul>
-                    <div class="card-footer">${genre}</div>
-                </div>
-            `)
+                `)
+            })
         })
-    })
+    }, 1500)
+}
